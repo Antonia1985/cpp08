f1d86129a78364cd278ec7154cf1a15fb7a8665b
Antonia <33874536+Antonia1985@users.noreply.github.com>
2026-06-11 13:02:28 +0200

    added more tests

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,6 +6,7 @@ int main()
 {
 
    // given test
+    std::cout << "---------Given Test---------" << std::endl;
     MutantStack<int> mstack;
     mstack.push(5);
     mstack.push(17);
@@ -31,9 +32,48 @@ int main()
     std::stack<int> s(mstack);
     // 
     std::cout << s.size() << std::endl; //5
+    std::cout << std::endl;
 
+
+    //test const
+    std::cout << "---------Test Const Iterator---------" << std::endl;
+    const MutantStack<int> constMStack(mstack);
+    MutantStack<int>::const_iterator cit = constMStack.begin();
+    MutantStack<int>::const_iterator cite = constMStack.end();
+    
+    while (cit != cite)
+    {
+        std::cout << *cit << std::endl;
+        ++cit;
+    }// 5 3 5 737 0
+
+    //test Copy constructor independence
+    std::cout << "---------Copy constructor independence---------" << std::endl;
+    MutantStack<int> mStack1(mstack);
+    mStack1.push(10);
+    mStack1.push(20);
+    mStack1.push(30);
+    
+    MutantStack<int>::iterator it1 = mStack1.begin();
+    MutantStack<int>::iterator ite1 = mStack1.end();
+    std::cout << "----mStack1: " <<std::endl;
+    while (it1 != ite1)
+    {
+        std::cout << *it1 << std::endl;
+        ++it1;
+    }// 5 3 5 737 0 10 20 30
+    
+    std::cout << "----mstack: " <<std::endl;
+    MutantStack<int>::iterator ito = mstack.begin();
+    MutantStack<int>::iterator iteo = mstack.end();
+    while (ito != iteo)
+    {
+        std::cout << *ito << std::endl;
+        ++ito;
+    }// 5 3 5 737 0
 /* 
     // test with list
+    std::cout << "---------Test with List---------" << std::endl;
     Mutantlist<int> mlist;
     mlist.push_back(5);
     mlist.push_back(17);
